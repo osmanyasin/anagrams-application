1414f33d5164718438a0e11c7266d777d4a7c842
Osman Yasin <osmanyasin0077@gmail.com>
2026-04-10 21:16:13 +0200

    fix: remove deprecated cors config

--- anagrams-service/src/main/java/com/bsg_selectathon/anagrams_service/config/CorsConfig.java
+++ anagrams-service/src/main/java/com/bsg_selectathon/anagrams_service/config/CorsConfig.java
@@ -16,10 +16,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
-                .allowedMethods(allowedMethods)
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "DELETE")
+                .allowedHeaders("*");
     }
 }
